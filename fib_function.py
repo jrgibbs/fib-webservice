741cc09413189515530b2ecc74837fc54d6d16ae
Joe Gibbs <joseph.gibbs@emc.com>
2015-05-23 13:58:24 -0400

    added additional test/exception for: negative numbers floats alpha characters

--- fib_function.py
+++ fib_function.py
@@ -2,6 +2,13 @@ __author__ = 'jgibbs'
 
 
 def fib(n):
+    if n < 1:
+        raise Exception("Invalid input: n must be >=1")
+    if n == 1:
+        return [0]
+    if n == 2:
+        return [0, 1]
+
     sequence = [0, 1]
     for i in range(3, n + 1):
         sequence.append(sequence[i-3] + sequence[i-2])
--- fib_test.py
+++ fib_test.py
@@ -7,10 +7,20 @@ class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.seq = [0, 1, 1, 2, 3, 5]
 
-    def test_something(self):
+    def test_expected_results(self):
         result = fib(6)
         print result
         self.assertEqual(self.seq, result)
+
+    def test_negative_number(self):
+        self.assertRaises(Exception, -5)
+
+    def test_float(self):
+        self.assertRaises(Exception, 3.14)
+
+    def test_alpha(self):
+        self.assertRaises(Exception, "foo")
+
 
 
 if __name__ == '__main__':
