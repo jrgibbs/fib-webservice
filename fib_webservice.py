1f399f74250c9e925b85e824509387961550b90c
Joe Gibbs <joseph.gibbs@emc.com>
2015-05-23 17:22:04 -0400

    corrected flask syntax.

--- fib_webservice.py
+++ fib_webservice.py
@@ -4,4 +4,11 @@ from fib_function import fib
 from flask import Flask
 from flask.json import jsonify
 
+app = Flask(__name__)
 
+@app.route('/fib/<int:n>', methods=['GET'])
+def fib_webservice(n):
+    return jsonify(sequence=fib(n))
+
+if __name__ == '__main__':
+    app.run(debug=True)
