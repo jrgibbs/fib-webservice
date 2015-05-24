161a07e268f269feeb91c72d94ea0725c9525c73
Joe Gibbs <joseph.gibbs@emc.com>
2015-05-23 21:33:17 -0400

    added additional docstrings

--- fib_webservice.py
+++ fib_webservice.py
@@ -8,10 +8,21 @@ app = Flask(__name__)
 
 @app.route('/fib/<int:n>', methods=['GET'])
 def fib_webservice(n):
+    """ This function provides output from the fib_function
+
+    :param n: A positive integer
+    :return: A dictionary with a list containing all of the numbers that
+    make the fibonacci sequence through n positions back to the web browser.
+    """
     return jsonify(sequence=fib(n))
 
 @app.errorhandler(404)
 def page_not_found(error):
+    """ This function generates an error for an invalid webservice call
+
+    :param error: 404: HTTP response code
+    :return: An error due to invalid input for the fib_webservice function call
+    """
     return jsonify(error=error.description, code=error.code, name=error.name)
 
 if __name__ == '__main__':
